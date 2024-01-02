aa4e464232272655feb2b229d3253dda9ae7226f
simonyang <simonyang777@qq.com>
2024-01-02 08:45:28 +0800

    rename variable

--- server/api/nce/book.get.ts
+++ server/api/nce/book.get.ts
@@ -2,8 +2,8 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 
 export default defineEventHandler(async (event) => {
-  const { num } = getQuery(event)
-  const filePath = path.resolve('assets', 'nce', `book${num}.json`)
+  const { book } = getQuery(event)
+  const filePath = path.resolve('assets', 'nce', `book${book}.json`)
   const json = await fs.readFile(filePath, 'utf8')
   return json
 })
