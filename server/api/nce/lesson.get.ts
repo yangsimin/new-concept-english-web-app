8944ebd277bb773b5c6ad740a368099cd0ca069a
null <null>
2024-01-02 10:31:20 +0800

    重构代码

--- server/api/nce/lesson.get.ts
+++ server/api/nce/lesson.get.ts
@@ -5,5 +5,5 @@ export default defineEventHandler(async (event) => {
   const { book, lessonId } = getQuery(event)
   const filePath = path.resolve('assets', 'nce', `book${book}`, `${lessonId}.json`)
   const json = await fs.readFile(filePath, 'utf8')
-  return json
+  return JSON.parse(json)
 })
