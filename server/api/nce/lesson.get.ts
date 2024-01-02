68f781083958a55d561cd507e8987f412bfb6eea
simonyang <simonyang777@qq.com>
2024-01-02 22:31:53 +0800

    move json files to public folder

--- server/api/nce/lesson.get.ts
+++ server/api/nce/lesson.get.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 
 export default defineEventHandler(async (event) => {
   const { book, lessonId } = getQuery(event)
-  const filePath = path.resolve('assets', 'nce', `book${book}`, `${lessonId}.json`)
+  const filePath = path.join('public', 'nce', `book${book}`, `${lessonId}.json`)
   const json = await fs.readFile(filePath, 'utf8')
   return JSON.parse(json)
 })
