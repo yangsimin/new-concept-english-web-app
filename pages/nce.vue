045e25bf58ad9dba2b2e7aca58733453e49590f1
simonyang <simonyang777@qq.com>
2024-01-02 08:45:06 +0800

    add todo-list

--- pages/nce.vue
+++ pages/nce.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+// todo 0. 完善切换下一课,下一本书的逻辑
+// todo 1. 增加 mp3 播放
+// todo 2. 拆分句子, 增加间隔
+// todo 3. 按键映射
+// todo 4. 重构代码
+// todo 5. 移除 mp3 的 git 同步
+// todo 6. server 异常处理
+
 import type { LocationQueryValue } from 'vue-router'
 
 const route = useRoute()
@@ -7,6 +15,7 @@ const router = useRouter()
 const book = ref<LocationQueryValue>()
 const lessonId = ref<LocationQueryValue>()
 const sentenceBlocks = ref<any>([])
+const lessonList = ref([])
 
 const validSentenceBlocks = computed(() => sentenceBlocks.value.slice(1))
 const hideEnText = ref(true)
@@ -15,6 +24,16 @@ const currentLine = computed(() => validSentenceBlocks.value[lineIndex.value])
 
 watch(lineIndex, () => {
   hideEnText.value = true
+})
+
+// todo update lessonList
+watch(book, async () => {
+  console.log('book change', book.value)
+  const { data } = await useFetch('/api/nce/book', {
+    query: {
+      book: book.value,
+    },
+  })
 })
 
 watchEffect(async () => {
@@ -66,20 +85,27 @@ function onClickNext() {
     lineIndex.value++
   }
 }
+
+function onClickNextLesson() {
+  router.push({
+    path: route.path,
+    query: {
+      book: 2,
+      lessonId: 2021,
+    },
+  })
+}
 </script>
 
 <template>
   <div v-if="sentenceBlocks.length > 0" flex="~ col" box-border h-100vh min-h-600px min-w-768px p="x-4 y-4" font-sans>
     <header h-100px>
-      <h2>
+      <h2 mt-0>
         新概念英语 1
-        <br>
-        Lesson {{ Number(lessonId) % 1000 }}
       </h2>
-      <div pl-2em text-xl capitalize>
+      <div w-max pl-2em text="right xl" capitalize>
         {{ sentenceBlocks[0].Sentence ?? '-' }}
-      </div>
-      <div pl-2em text-xl>
+        <br>
         {{ sentenceBlocks[0].Sentence_cn ?? '-' }}
       </div>
     </header>
@@ -97,6 +123,10 @@ function onClickNext() {
           </button>
           <button @click="onClickNext">
             Next
+          </button>
+          <br>
+          <button @click="onClickNextLesson">
+            Next Lesson
           </button>
         </div>
       </article>
