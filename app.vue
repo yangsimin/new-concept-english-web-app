18c99533ab60bc32fde9d85b1ddcbf3f18a2c8c0
simonyang <simonyang777@qq.com>
2024-01-02 22:10:15 +0800

    add page transition

--- app.vue
+++ app.vue
@@ -10,4 +10,13 @@
 html {
   @apply font-sans;
 }
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.3rem);
+}
 </style>
