4b27431074b0cfd4e9c236a14d8dd514b81d7614
simonyang <simonyang777@qq.com>
2024-01-02 23:38:06 +0800

    bugfix: server failed to read file

--- server/api/nce/book.get.ts
+++ server/api/nce/book.get.ts
@@ -3,7 +3,5 @@ import path from 'node:path'
 
 export default defineEventHandler(async (event) => {
   const { book } = getQuery(event)
-  const filePath = path.join('public', 'nce', `book${book}.json`)
-  const json = await fs.readFile(filePath, 'utf8')
-  return JSON.parse(json)
+  await sendRedirect(event, `/nce/book${book}.json`)
 })
