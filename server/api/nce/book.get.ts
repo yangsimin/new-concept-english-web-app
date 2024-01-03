55f23c34ef6e018f88ae51fcae9dd42fe08aed99
yangsimin <121758638@qq.com>
2024-01-03 10:50:52 +0800

    删除冗余代码

--- server/api/nce/book.get.ts
+++ server/api/nce/book.get.ts
@@ -1,6 +1,3 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
-
 export default defineEventHandler(async (event) => {
   const { book } = getQuery(event)
   await sendRedirect(event, `/nce/book${book}.json`)
--- server/api/nce/lesson.get.ts
+++ server/api/nce/lesson.get.ts
@@ -1,6 +1,3 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
-
 export default defineEventHandler(async (event) => {
   const { book, lessonId } = getQuery(event)
   await sendRedirect(event, `/nce/book${book}/${lessonId}.json`)
