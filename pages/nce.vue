d28f86b7d1a06141af764ec37b298c04e7645850
simonyang <simonyang777@qq.com>
2024-01-06 16:12:03 +0800

    抽取 LessonModeListening 组件

--- pages/nce.vue
+++ pages/nce.vue
@@ -7,7 +7,7 @@
 // done 5. 移除 mp3 的 git 同步
 // todo 6. server 异常处理
 
-interface Lesson {
+export interface Lesson {
   id: number
   titleEn: string
   titleZh: string
@@ -15,7 +15,7 @@ interface Lesson {
   audioUrl: string
 }
 
-interface Sentence {
+export interface Sentence {
   startAt: number
   stopAt: number
   en: string
@@ -29,37 +29,6 @@ const bookId = ref(1)
 const lessonId = ref(1001)
 const lessonIdList = ref<number[]>([])
 const currentLesson = ref<Lesson | undefined>()
-const sentenceIndex = ref(0)
-const currentSentence = ref<Sentence | undefined>()
-
-const isSoundEnable = ref(true)
-const isEnTextHidden = ref(true)
-const { audioInstance, playAudio, pauseAudio, updateSource } = useAudio()
-
-const keyFnMap: Record<string, { name: string, fn: Function }> = {
-  j: {
-    name: '下一步',
-    fn: () => { onClickNextStep() },
-  },
-  k: {
-    name: '上一句',
-    fn: () => { onClickPrevSentence() },
-  },
-  l: {
-    name: '下一课',
-    fn: () => { stepLesson(1) },
-  },
-  h: {
-    name: '上一课',
-    fn: () => { stepLesson(-1) },
-  },
-  m: {
-    name: '静音切换',
-    fn: () => { isSoundEnable.value = !isSoundEnable.value },
-  },
-}
-
-addListenKeyDown()
 
 watchEffect(async () => {
   if (!Array.isArray(route.query.book)) {
@@ -77,16 +46,6 @@ watchEffect(async () => {
   }
 })
 
-watchEffect(() => {
-  currentSentence.value = currentLesson.value?.sentences[sentenceIndex.value]
-  isEnTextHidden.value = true
-  pauseAudio()
-})
-
-watchEffect(() => {
-  audioInstance.muted = !isSoundEnable.value
-})
-
 watchEffect(async () => {
   const lessonIds = await requestBook(bookId.value) ?? []
   lessonIdList.value = lessonIds.filter(id => id % 2)
@@ -102,29 +61,7 @@ watchEffect(async () => {
   }
 
   currentLesson.value = await requestLesson(bookId.value, lessonId.value) ?? undefined
-  if (!currentLesson.value) {
-    return
-  }
-
-  sentenceIndex.value = 0
-  isEnTextHidden.value = true
-  updateSource(currentLesson.value.audioUrl)
 })
-
-function addListenKeyDown() {
-  const onKeyDown = (event: KeyboardEvent) => {
-    const key = event.key
-    if (keyFnMap[key]) {
-      keyFnMap[key].fn()
-    }
-  }
-  onMounted(() => {
-    window.addEventListener('keydown', onKeyDown)
-  })
-  onUnmounted(() => {
-    window.removeEventListener('keydown', onKeyDown)
-  })
-}
 
 async function requestLesson(book: number, lessonId: number): Promise<Lesson | null> {
   const { data } = await useFetch('/api/nce/lesson', {
@@ -153,25 +90,6 @@ async function requestBook(book: number): Promise<number[] | null> {
   return data.value
 }
 
-function onClickPrevSentence() {
-  if (sentenceIndex.value > 0) {
-    sentenceIndex.value--
-  }
-}
-
-function onClickNextStep() {
-  if (isEnTextHidden.value) {
-    isEnTextHidden.value = false
-    if (isSoundEnable.value) {
-      playAudio(Number(currentSentence.value!.startAt), Number(currentSentence.value!.stopAt))
-    }
-    return
-  }
-  if (sentenceIndex.value < currentLesson.value!.sentences.length - 1) {
-    sentenceIndex.value++
-  }
-}
-
 function stepLesson(step: number) {
   const nextLessonIndex = lessonIdList.value.findIndex(id => id === lessonId.value) + step
   if (nextLessonIndex < 0 || nextLessonIndex > lessonIdList.value.length - 1) {
@@ -185,12 +103,16 @@ function stepLesson(step: number) {
     },
   })
 }
+
+function toggleLessonMode() {
+
+}
 </script>
 
 <template>
   <div flex="~ col" box-border p="x-4 y-4">
-    <header flex items-center justify-between>
-      <div flex text="2xl">
+    <header flex items-center justify-start gap-2>
+      <div flex text="2xl" flex-1>
         <strong mr-4>
           {{ `${bookId}-${lessonId % 1000}` }}
         </strong>
@@ -202,59 +124,27 @@ function stepLesson(step: number) {
           {{ currentLesson?.titleZh }}
         </span>
       </div>
+      <button hover="bg-gray-400/20" rounded p1 text-3xl transition-200 @click="toggleLessonMode">
+        <span i="carbon-apple" />
+      </button>
       <LessonMenu
         :current-lesson-id="lessonId"
         :lesson-id-list="lessonIdList"
         @select-lesson-id="lessonId = $event"
       />
     </header>
-    <main v-if="currentSentence" mt-20>
-      <article flex="~ col" items-center justify-center gap-8>
-        <p relative flex items-center text-4xl>
-          {{ currentSentence.zh }}
-          <label absolute right-0 mr--16 cursor-pointer text-2xl>
-            <input v-model="isSoundEnable" type="checkbox" hidden>
-            <span>{{ isSoundEnable ? '🔊' : '🔇' }}</span>
-          </label>
-        </p>
-        <p flex gap-2 text-4xl>
-          <span
-            v-for="(piece, index) in currentSentence.en.trim().split(/\s/)"
-            :key="index"
-            py-1
-            border-b="4 solid sky-500"
-          >
-            <span :class="{ 'opacity-0': isEnTextHidden }" px-2>{{ piece }}</span>
-          </span>
-        </p>
-      </article>
-      <div grid="~ cols-[repeat(2,115px)]" mt-14 place-content-center gap-20px>
-        <button class="btn" :disabled="sentenceIndex === 0" @click="onClickPrevSentence">
-          上一句
-        </button>
-        <button class="btn" :disabled="sentenceIndex === currentLesson!.sentences.length - 1 && !isEnTextHidden" @click="onClickNextStep">
-          下一步
-        </button>
-        <button class="btn" :disabled="lessonId % 1000 <= 1" @click="stepLesson(-1)">
-          上一课
-        </button>
-        <button class="btn" :disabled="lessonId % 1000 >= lessonIdList.length - 1" @click="stepLesson(1)">
-          下一课
-        </button>
-        <div v-for="({ name }, key) in keyFnMap" :key="key">
-          <span text="center white" mr-2 inline-block w-30px rounded bg-sky-500>{{ key }}</span>{{ name }}
-        </div>
-      </div>
+    <main mt-20>
+      <LessonModeListening
+        v-if="currentLesson"
+        :current-lesson="currentLesson"
+        @next-lesson="stepLesson(1)"
+        @prev-lesson="stepLesson(-1)"
+      />
     </main>
   </div>
 </template>
 
 <style scoped>
-.btn {
-  @apply px-4 py-px rounded bg-sky-500 text-white border-(sky-700 2px) hover:bg-opacity-80
-  disabled:(cursor-default bg-gray-600 opacity-50 hover:bg-opacity-100);
-}
-
 .v-enter-active,
 .v-leave-active {
   transition: all 0.3s;
