8a251f2e992c820e1ff925c3703663e65c34f0e4
yangsimin <121758638@qq.com>
2024-01-08 21:13:23 +0800

    引入 diff 比对答案

--- components/LessonModeWriting.vue
+++ components/LessonModeWriting.vue
@@ -1,27 +1,23 @@
 <script setup lang="ts">
+import * as Diff from 'diff'
 import type { Lesson, Sentence } from '~/pages/nce.vue'
 
 interface SentenceInfo {
   isAnswerVisible: boolean
   inputText: string
   sentence: Sentence
-  status: 'pass' | 'fail' | 'unknown'
+  diffChanges?: Diff.Change[]
 }
 
 const props = defineProps<{
   currentLesson: Lesson
 }>()
 
-const statusColor: Record<SentenceInfo['status'], string> = {
-  pass: 'text-green-500',
-  fail: 'text-red-500',
-  unknown: '',
-}
-
 const { currentLesson } = toRefs(props)
 
 const formData = ref<SentenceInfo[]>([])
 const isSubmited = ref(false)
+const [isAllVisible, toggleVisible] = useToggle(false)
 
 watchEffect(() => {
   formData.value = currentLesson.value.sentences.map((sentence) => {
@@ -29,73 +25,77 @@ watchEffect(() => {
       isAnswerVisible: false,
       inputText: '',
       sentence,
-      status: 'unknown',
     }
+  })
+})
+
+watch(isAllVisible, () => {
+  formData.value = formData.value.map((item) => {
+    item.isAnswerVisible = isAllVisible.value
+    return item
   })
 })
 
 function onSubmit() {
   isSubmited.value = true
+  isAllVisible.value = true
   formData.value = formData.value.map((item) => {
-    item.isAnswerVisible = true
-    item.status = checkResult(item)
-    return item
-  })
-}
-
-function onReset() {
-  isSubmited.value = false
-  formData.value = formData.value.map((item) => {
-    item.isAnswerVisible = false
+    item.diffChanges = checkResult(item)
     return item
   })
 }
 
 function onClear() {
   isSubmited.value = false
+  isAllVisible.value = false
   formData.value = formData.value.map((item) => {
-    item.isAnswerVisible = false
     item.inputText = ''
-    item.status = 'unknown'
+    item.diffChanges = []
     return item
   })
 }
 
-function checkResult(item: SentenceInfo): SentenceInfo['status'] {
-  if (item.inputText.trim() === item.sentence.en.trim()) {
-    return 'pass'
-  }
-  else {
-    return 'fail'
-  }
+function checkResult(item: SentenceInfo): Diff.Change[] {
+  return Diff.diffWords(item.inputText.trim(), item.sentence.en.trim())
 }
 </script>
 
 <template>
-  <article grid grid-cols-1 mx-auto min-w-300px w-fit gap-2rem lg:grid-cols-2>
+  <article grid grid-cols-1 mx-auto min-w-300px w-max gap-2rem text-lg xl:grid-cols-2>
     <div v-for="(eachItem, index) of formData" :key="eachItem.sentence.startAt">
       <p flex items-center>
         {{ index + 1 }}. {{ eachItem.sentence.zh }}
         <span
-          :icon="eachItem.isAnswerVisible ? 'carbon-view-filled' : ' carbon-view-off-filled'" ml-2
+          :icon="eachItem.isAnswerVisible ? 'carbon-view-filled' : ' carbon-view-off-filled'"
+          ml-2
           @mouseenter="!isSubmited && (eachItem.isAnswerVisible = true)"
           @mouseout="!isSubmited && (eachItem.isAnswerVisible = false)"
         />
       </p>
-      <div border-b="2px black" p-1 focus-within-border-b-sky-500>
+      <div border-b="2px black" pl-1 focus-within-border-b-sky-500>
         <input v-model="eachItem.inputText" type="text" w-full outline-none>
       </div>
-      <p p-1 :opacity="eachItem.isAnswerVisible ? 100 : 0" :class="[statusColor[eachItem.status]]">
-        {{ eachItem.sentence.en }}
-      </p>
+      <div pl-1 pr-20 :opacity="eachItem.isAnswerVisible ? 100 : 0">
+        <p v-if="!eachItem.diffChanges?.length">
+          {{ eachItem.sentence.en }}
+        </p>
+        <p v-else>
+          <span
+            v-for="(change, i) in eachItem.diffChanges" :key="i"
+            :class="{ 'text-green-500': !change.removed, 'text-red-500': change.added }"
+          >
+            {{ change.removed ? '' : change.value }}
+          </span>
+        </p>
+      </div>
     </div>
   </article>
-  <footer flex justify-center gap-4>
+  <footer mt-10 flex justify-center gap-4>
     <button class="btn-primary" @click="onSubmit">
       提交
     </button>
-    <button class="btn-primary" @click="onReset">
-      隐藏
+    <button class="btn-primary" @click="toggleVisible()">
+      {{ isAllVisible ? '隐藏' : '显示' }}
     </button>
     <button class="btn-primary" @click="onClear">
       清除
