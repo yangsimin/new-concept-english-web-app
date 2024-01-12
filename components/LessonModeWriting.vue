6924eba853d53c7aa1e935c4eae405bd5550d1f3
null <null>
2024-01-12 17:51:42 +0800

    添加 ChatGPT 提示词拷贝功能

--- components/LessonModeWriting.vue
+++ components/LessonModeWriting.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import * as Diff from 'diff'
+import { copyToClipboard } from '~/composables/utils'
 import type { Lesson, Sentence } from '~/pages/nce.vue'
 
 interface SentenceInfo {
@@ -105,20 +106,43 @@ function addListenKeyDown() {
     window.removeEventListener('keydown', onKeyDown)
   })
 }
+
+async function copySentencePrompt({ sentence, inputText }: SentenceInfo) {
+  const promptTemplate = `
+  请你扮演一位资深的英语教育者，下面请看下面三段文字：
+
+    1. ${sentence.zh}。
+    2. ${inputText}
+    3. ${sentence.en}
+
+  第一句是需要被翻译成英文的中文句子。
+  第二句是我所翻译的英文。
+  第三句是新概念教材的英语原文。
+
+  请对比我的翻译和新概念的原文的差异，分析差异点，并告诉我我的英语哪些方面薄弱，并指导我改进`
+
+  await copyToClipboard(promptTemplate)
+
+  const url = 'https://tongyi.aliyun.com/qianwen/'
+  // eslint-disable-next-line no-alert
+  alert(`已拷贝，请前往《通义千问》提问 ${url}`)
+}
 </script>
 
 <template>
   <article grid grid-cols-2 mx-auto min-w-300px w-max gap-2rem text-lg lt-xl:grid-cols-1>
     <div v-for="(eachItem, index) of formData" :key="eachItem.sentence.startAt">
-      <p flex items-center>
+      <div flex items-center>
         {{ index + 1 }}. {{ eachItem.sentence.zh }}
-        <span
-          :icon="eachItem.isAnswerVisible ? 'carbon-view-filled' : ' carbon-view-off-filled'"
-          ml-2
-          @mouseenter="!isSubmited && (eachItem.isAnswerVisible = true)"
-          @mouseout="!isSubmited && (eachItem.isAnswerVisible = false)"
-        />
-      </p>
+        <div ml-2 flex items-center gap-2>
+          <span
+            :icon="eachItem.isAnswerVisible ? 'carbon-view-filled' : ' carbon-view-off-filled'"
+            @mouseenter="!isSubmited && (eachItem.isAnswerVisible = true)"
+            @mouseout="!isSubmited && (eachItem.isAnswerVisible = false)"
+          />
+          <span icon="carbon-chat-bot" cursor-pointer title="复制提示词" @click="copySentencePrompt(eachItem)" />
+        </div>
+      </div>
       <div border-b="2px black" pl-1 focus-within-border-b-sky-500>
         <input v-model="eachItem.inputText" type="text" w-full outline-none>
       </div>
