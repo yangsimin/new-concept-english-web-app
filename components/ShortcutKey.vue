9b0d7796710f67a19f0aa41ef9061fb7c464c8b1
simonyang <simonyang777@qq.com>
2024-01-20 17:06:17 +0800

    修改快捷键组件样式

--- components/ShortcutKey.vue
+++ components/ShortcutKey.vue
@@ -19,7 +19,7 @@ function onKeyDown(event: KeyboardEvent) {
 </script>
 
 <template>
-  <div fixed bottom-0 left-0 right-0 my-4 flex flex-wrap items-center justify-center gap-x-10 gap-y-4 opacity-70>
+  <div gap="x-10 y-4" fixed bottom-0 left-0 right-0 flex flex-wrap items-center justify-center bg-sky-100 py-4 dark:bg-sky-800>
     <div v-for="({ name }, key) in keyFnMap" :key="key">
       <span text="center white" mr-2 inline-block rounded bg-sky-500 px-2>{{ key }}</span>
       {{ name }}
