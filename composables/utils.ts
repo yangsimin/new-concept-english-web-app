89ed95b032f4ec25ebe16e540093ede447fd23c6
yangsimin <121758638@qq.com>
2024-01-26 10:26:01 +0800

    调整 intend spaces

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,14 +1,13 @@
 export function throttle(fn: Function, delay: number) {
-    let timer: NodeJS.Timeout | undefined
-  
-    return function (...args: any[]) {
-      if (timer) {
-        return
-      }
-      fn.apply(this, args)
-      timer = setTimeout(() => {
-        timer = undefined
-      }, delay)
+  let timer: NodeJS.Timeout | undefined
+
+  return function (...args: any[]) {
+    if (timer) {
+      return
     }
+    fn.apply(this, args)
+    timer = setTimeout(() => {
+      timer = undefined
+    }, delay)
   }
-  
+}
